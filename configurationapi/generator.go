ee1a3ce48e0540a4ff9481248dcf5e0ed8b55454
wesleymccollam <wesleymccollam@pingidentity.com>
2023-09-26 11:04:35 -0400

    script call location

--- configurationapi/generator.go
+++ configurationapi/generator.go
@@ -2,4 +2,4 @@ package configurationapi
 
 // This file contains any go generate comments used by the client
 
-//go:generate ./scripts/generateTfsdkTags.py
+//go:generate ../scripts/generateTfsdkTags.py
