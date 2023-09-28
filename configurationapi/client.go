648c266b9d2950c68f0f70918fe8d584a04dc0e8
Henry Recker <henryrecker@pingidentity.com>
2023-09-28 13:53:12 -0500

    Remove debug logic

--- configurationapi/client.go
+++ configurationapi/client.go
@@ -450,26 +450,11 @@ func parameterToJson(obj interface{}) (string, error) {
 
 // callAPI do the request.
 func (c *APIClient) callAPI(request *http.Request) (*http.Response, error) {
-	if c.cfg.Debug {
-		dump, err := httputil.DumpRequestOut(request, true)
-		if err != nil {
-			return nil, err
-		}
-		log.Printf("\n%s\n", string(dump))
-	}
-
 	resp, err := c.cfg.HTTPClient.Do(request)
 	if err != nil {
 		return resp, err
 	}
 
-	if c.cfg.Debug {
-		dump, err := httputil.DumpResponse(resp, true)
-		if err != nil {
-			return resp, err
-		}
-		log.Printf("\n%s\n", string(dump))
-	}
 	return resp, err
 }
 
